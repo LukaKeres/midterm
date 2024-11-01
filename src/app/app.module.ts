e60ac9f6eea2457132627de69e10a7aab9950dd4
LukaKeres <113135258+LukaKeres@users.noreply.github.com>
2024-11-01 11:46:06 +0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UserComponent } from './Components/user/user.component';
 
@@ -12,6 +13,8 @@ import { UserComponent } from './Components/user/user.component';
   imports:[
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideClientHydration()
